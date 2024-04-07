34f9ccc450b434d6131d7861da541dbd8c79201d
Jakob Stornig <jastornig@edu.aau.at>
2024-04-07 12:18:57 +0200

    (test) added Tests

---
+++ src/test/java/com/group2/catanbackend/GameControllerTest.java
@@ -0,0 +1,77 @@
+package com.group2.catanbackend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group2.catanbackend.dto.JoinRequestDto;
+import com.group2.catanbackend.service.GameService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = CatanBackendApplication.class)
+@AutoConfigureMockMvc
+public class GameControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private GameService gameService;
+
+    @Test
+    public void testGamesFoundOnceCreated() throws Exception{
+        String createdGameID = gameService.createGame();
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/catan/game/list"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(createdGameID)));
+    }
+
+    @Test
+    public void testCannotJoinNotExistingGame() throws Exception {
+        JoinRequestDto requestDto = new JoinRequestDto("a", "asdf");
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/catan/game/connect")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestDto))
+                )
+                .andExpect(status().is(404)
+
+        );
+    }
+
+    @Test
+    public void testCanJoinExistingGame() throws Exception {
+        String gameID = gameService.createGame();
+        JoinRequestDto requestDto = new JoinRequestDto("player", gameID);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                        .post("/catan/game/connect")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(requestDto))
+        )
+                .andExpect(status().isOk());
+
+        Assertions.assertEquals(1, gameService.getGames().size());
+        Assertions.assertEquals(1, gameService.getGames().get(0).getPlayerCount());
+    }
+
+    private String toJson(final Object obj){
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+}
--- src/test/java/com/group2/catanbackend/WebSocketBrokerIntegrationTest.java
+++ src/test/java/com/group2/catanbackend/WebSocketBrokerIntegrationTest.java
@@ -1,18 +1,24 @@
 package com.group2.catanbackend;
 
+import com.group2.catanbackend.dto.JoinRequestDto;
+import com.group2.catanbackend.service.TokenService;
 import com.group2.catanbackend.websocket.StompFrameHandlerClientImpl;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.converter.StringMessageConverter;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
+import java.util.UUID;
 import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,8 +29,11 @@ class WebSocketBrokerIntegrationTest {
     @LocalServerPort
     private int port;
 
-    private final String WEBSOCKET_URI = "ws://localhost:%d/websocket-example-broker";
-    private final String WEBSOCKET_TOPIC = "/topic/hello-response";
+    @Autowired
+    private TokenService tokenService;
+
+    private final String WEBSOCKET_URI = "ws://localhost:%d/catan";
+    private final String WEBSOCKET_TOPIC_PREFIX = "/topic/game/";
 
     /**
      * Message Queue
@@ -32,28 +41,85 @@ class WebSocketBrokerIntegrationTest {
     BlockingQueue<String> messages = new LinkedBlockingQueue<>();
 
     @Test
-    public void testWebSocketMessageBroker() throws Exception {
-        StompSession session = initStompSession();
+    public void testProtocolUpgradeAllowedIfValidToken() throws Exception{
+        JoinRequestDto dto = new JoinRequestDto("player", "game");
+        String token = UUID.randomUUID().toString();
+        tokenService.pushToken(token, dto);
 
-        String message = "Test message";
-        session.send("/app/hello", message);
+        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+        headers.add("authorization", token);
 
-        var expectedResponse = "echo from broker: " + message;
-        assertThat(messages.poll(1, TimeUnit.SECONDS)).isEqualTo(expectedResponse);
+        WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        StompSession session = stompClient.connectAsync(String.format(WEBSOCKET_URI, port), headers, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
+
+        Assertions.assertTrue(session.isConnected());
     }
 
+    @Test
+    public void testProtocolUpgradeNotAllowedIfTokenInvalid(){
+        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+        headers.add("authorization", "invalidToken");
 
-    public StompSession initStompSession() throws Exception{
+        WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        Assertions.assertThrows(Exception.class, ()->{
+            stompClient.connectAsync(String.format(WEBSOCKET_URI, port),
+                    headers,
+                    new StompSessionHandlerAdapter() {
+        })
+                    .get(1, TimeUnit.SECONDS);});
+
+    }
+
+    @Test
+    public void testProtocolUpgradeNotAllowedIfNoTokenPresent(){
+        WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        Assertions.assertThrows(Exception.class, ()->{
+            stompClient.connectAsync(String.format(WEBSOCKET_URI, port),
+                            new StompSessionHandlerAdapter() {
+                            })
+                    .get(1, TimeUnit.SECONDS);});
+
+    }
+
+    @Test
+    public void testSubscriptionAllowedIfPlayerIsInGame() throws Exception{
+        String gameID = "GameID";
+
+        StompSession session = initValidSession("Player", gameID);
+
+        String topic = WEBSOCKET_TOPIC_PREFIX + gameID + "/messages";
+        session.subscribe(topic, new StompFrameHandlerClientImpl(messages));
+        String message = "hello";
+        session.send(topic, message);
+        assertThat(messages.poll(1, TimeUnit.SECONDS)).isEqualTo(message);
+    }
+
+    @Test
+    public void testSubscriptionNotAllowedIfPlayerIsNotInGame() throws Exception{
+        String gameID = "GameID";
+
+        StompSession session = initValidSession("Player", gameID);
+
+        String topic = WEBSOCKET_TOPIC_PREFIX + "notGameID" + "/messages";
+        session.subscribe(topic, new StompFrameHandlerClientImpl(messages));
+        String message = "hello";
+        session.send(topic, message);
+        assertThat(messages.poll(1, TimeUnit.SECONDS)).isNull();
+    }
+
+    public StompSession initValidSession(String playerName, String gameID) throws Exception{
+        JoinRequestDto dto = new JoinRequestDto(playerName, gameID);
+        String token = UUID.randomUUID().toString();
+        tokenService.pushToken(token, dto);
+
+        WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+        headers.add("authorization", token);
+
         WebSocketStompClient stompClient = new WebSocketStompClient(new StandardWebSocketClient());
         stompClient.setMessageConverter(new StringMessageConverter());
-
-        StompSession session = stompClient.connectAsync(String.format(WEBSOCKET_URI, port),
-                new StompSessionHandlerAdapter() {
-                })
-                .get(1, TimeUnit.SECONDS);
-
-        session.subscribe(WEBSOCKET_TOPIC, new StompFrameHandlerClientImpl(messages));
-
+        StompSession session = stompClient.connectAsync(String.format(WEBSOCKET_URI, port), headers, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
         return session;
     }
 }
