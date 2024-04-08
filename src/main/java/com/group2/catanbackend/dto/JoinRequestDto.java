eccf7e2286ee80cb10b166416ff56acf03d8ae30
Jakob Stornig <jastornig@edu.aau.at>
2024-04-08 13:42:27 +0200

    refactoring: removed data annotation

--- src/main/java/com/group2/catanbackend/dto/JoinRequestDto.java
+++ src/main/java/com/group2/catanbackend/dto/JoinRequestDto.java
@@ -2,13 +2,12 @@ package com.group2.catanbackend.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
 public class JoinRequestDto {
     @NotNull
     @NotEmpty
