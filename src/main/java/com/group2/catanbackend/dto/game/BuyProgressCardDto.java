856b6f29dc860dc979b1a1d2fe745f9d3cf04abf
JL <lujordan@edu.aau.at>
2024-06-07 16:10:48 +0200

    feature: created DTO to handle the gamemove of buying progresscards

---
+++ src/main/java/com/group2/catanbackend/dto/game/BuyProgressCardDto.java
@@ -0,0 +1,15 @@
+package com.group2.catanbackend.dto.game;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class BuyProgressCardDto extends GameMoveDto {
+
+    public BuyProgressCardDto() {
+        this.setEventType(GameMoveType.BUYPROGRESSCARD);
+    }
+}
